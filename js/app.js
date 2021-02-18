51cdb6a2caf94d6b91e3f09c0ae1576814c21936
MarwanReyadAL-Khawaldeh <marwanreyad@gmail.com>
2021-02-18 21:54:17 +0200

    pair programming

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 
 
 let userName = prompt('Please enter your name?');
-//console.log(userName);
+console.log(userName);
 alert('Welcome' + ' ' + userName);
 
 
@@ -11,183 +11,204 @@ let counter = 0;
 
 
 let userAge = prompt('Enter your age please?');
-//console.log(userAge);
+console.log(userAge);
 alert('your age is :' + ' ' + userAge);
 
 
 
 
-const niceFriend= function(){
-let goodFrined = prompt('Do you think I am a good friend? :YES/NO');
-if (goodFrined.toLocaleLowerCase() === 'yes') {
-  alert('Thank you Bro');
-  counter = counter + 1;
+const niceFriend = function () {
+  let goodFrined = prompt('Do you think I am a good friend? :YES/NO');
+  while (goodFrined.toLocaleLowerCase() === 'no' && goodFrined.toLocaleLowerCase() === 'n'||goodFrined.toLocaleLowerCase()==='yes'||goodFrined.toLocaleLowerCase()==='y') {
 
-  //console.log(goodFrined);
-} else if (goodFrined.toLocaleLowerCase() === 'no') {
-  alert('I am sorry that I am not a good friend');
-  //console.log(goodFrined);
-}
+    if (goodFrined.toLocaleLowerCase() === 'yes' || goodFrined.toLocaleLowerCase() === 'y') {
+
+      alert('Thank you Bro');
+      counter = counter + 1;
+      break;
+
+    } else {
+      alert('I am sorry that I am not a good friend');
+      break;
+
+    }
+  }
 };
+
 niceFriend();
 
-const coffeeLover=function(){
-let loveCoffe = prompt('Do i love coffe? :YES/NO');
+const coffeeLover = function () {
+  let loveCoffe = prompt('Do i love coffe? :YES/NO');
+  while (loveCoffe.toLocaleLowerCase() === 'no' && loveCoffe.toLocaleLowerCase() === 'n'||loveCoffe.toLocaleLowerCase()==='yes'||loveCoffe.toLocaleLowerCase()==='y') {
 
-if (loveCoffe.toLowerCase() === 'yes') {
-  alert('you are good');
-  counter = counter + 1;
+    if (loveCoffe.toLowerCase() === 'yes' || loveCoffe.toLocaleLowerCase() === 'y') {
+      alert('you are good');
+      counter = counter + 1;
+      break;
 
-  //console.log(loveCoffe);
-} else if (loveCoffe.toLowerCase() === 'no') {
-  alert('you are wrong I love coffee very much');
-  //console.log(loveCoffe);
-}
-}
+
+    } else {
+      alert('you are wrong I love coffee very much');
+      break;
+
+    }
+  }
+};
 coffeeLover();
 
-const career = function(){
-let ownBusiness = prompt('Do you think I can start my own business in the future? :YES/NO');
+const career = function () {
+  let ownBusiness = prompt('Do you think I can start my own business in the future? :YES/NO');
+  while (ownBusiness.toLocaleLowerCase() !== 'no' || ownBusiness.toLocaleLowerCase() !== 'n'||ownBusiness.toLocaleLowerCase()==='yes'||ownBusiness.toLocaleLowerCase()==='y') {
 
-if (ownBusiness.toLowerCase() === 'yes') {
-  alert('Thank you for holding that I can do it');
-  counter = counter + 1;
+    if (ownBusiness.toLowerCase() === 'yes' || ownBusiness.toLocaleLowerCase() === 'y') {
+      alert('Thank you for holding that I can do it');
+      counter = counter + 1;
+      break;
 
-  //console.log(ownBusiness);
-}
-else if (ownBusiness.toLowerCase() === 'no') {
-  alert('So what should I do');
-  //console.log(ownBusiness);
-}
+      //console.log(ownBusiness);
+    }
+    else {
+      alert('So what should I do');
+      break
+      //console.log(ownBusiness);
+    }
+  }
 };
 career();
-const swimming = function(){
-let toSwim = prompt('Am I good at swimming? :YES/NO');
+const swimming = function () {
+  let toSwim = prompt('Am I good at swimming? :YES/NO');
+  while (toSwim.toLocaleLowerCase() === 'no' || toSwim.toLocaleLowerCase() === 'n'||toSwim.toLocaleLowerCase()==='yes'||toSwim.toLocaleLowerCase()==='y') {
 
-if (toSwim.toLocaleLowerCase() === 'yes') {
-  alert('That right ');
-  counter = counter + 1;
-
-  //console.log(toSwim)
-} else if (toSwim.toLocaleLowerCase() === 'no') {
-  alert('you are wrong');
-  //console.log(toSwim);
-}
+    if (toSwim.toLocaleLowerCase() === 'yes' || toSwim.toLocaleLowerCase() === 'y') {
+      alert('That right ');
+      counter = counter + 1;
+      break;
+      //console.log(toSwim)
+    } else {
+      alert('you are wrong');
+      break;
+      //console.log(toSwim);
+    }
+  }
 };
 swimming();
 
-const helpOthers = function(){
-let asksHelp = prompt('If a stranger suddenly came to me and asks for help, would I help him?? :YES/NO');
-if (asksHelp.toLocaleLowerCase() === 'yes') {
-  alert('Thats right we must help other if we can');
-  counter = counter + 1;
+const helpOthers = function () {
+  let asksHelp = prompt('If a stranger suddenly came to me and asks for help, would I help him?? :YES/NO');
+  while (asksHelp.toLocaleLowerCase() === 'no' || asksHelp.toLocaleLowerCase() === 'n'||asksHelp.toLocaleLowerCase()==='yes'||asksHelp.toLocaleLowerCase()==='y') {
 
-  //console.log(asksHelp);
-} else if (asksHelp.toLocaleLowerCase() === 'no') {
-  alert('you are wrong man ');
-  //console.log(asksHelp);
-}
+    if (asksHelp.toLocaleLowerCase() === 'yes' || asksHelp.toLocaleLowerCase() === 'y') {
+      alert('Thats right we must help other if we can');
+      counter = counter + 1;
+      break;
+      //console.log(asksHelp);
+    } else if (asksHelp.toLocaleLowerCase() === 'no') {
+      alert('you are wrong man ');
+      break
+      //console.log(asksHelp);
+    }
 
+  }
 };
 helpOthers();
-const guessNumbers = function(){
-let num = 0;
-let gussNumber;
-while (num < 4) {
-  gussNumber = prompt('Guess a Number');
+const guessNumbers = function () {
+  let num = 0;
+  let gussNumber;
+  while (num < 4) {
+    gussNumber = prompt('Guess a Number');
 
-  if (gussNumber < 7) {
-    alert('Too Low');
-    console.log(gussNumber);
+    if (gussNumber < 7) {
+      alert('Too Low');
+      console.log(gussNumber);
 
 
+    }
+    else if (gussNumber > 7) {
+      alert('Too High');
+      console.log(gussNumber);
+
+
+    }
+    else if (gussNumber === '7') {
+      alert('It is true');
+      console.log(gussNumber);
+      counter = counter + 1;
+
+      break;
+
+    }
+
+
+    ++num;
   }
-  else if (gussNumber > 7) {
-    alert('Too High');
-    console.log(gussNumber);
 
-
+  if (gussNumber !== '7') {
+    alert('You have exhausted all your attempts,The correct answer is 7');
   }
-  else if (gussNumber === '7') {
-    alert('It is true');
-    console.log(gussNumber);
-    counter = counter + 1;
-
-    break;
-
-  }
-
-
-  ++num;
-}
-
-if (gussNumber !== '7') {
-  alert('You have exhausted all your attempts,The correct answer is 7');
-}
-}
+};
 
 guessNumbers();
 
-const mySport = function(){
-let arr = ['football', 'swimming', 'basketball', 'tennis', 'golf', 'boxing', 'bowling', 'horse racing', 'judo'];
+const mySport = function () {
+  let arr = ['football', 'swimming', 'basketball', 'tennis', 'golf', 'boxing', 'bowling', 'horse racing', 'judo'];
 
-let gussSport;
-let guss = 0;
-while (guss < 6) {
-  gussSport = prompt('Guess What is my favorite sport?');
-  if (gussSport === arr[0]) {
-    alert('Correct the Football is my favorite sport');
-    counter = counter + 1;
-    break;
+  let gussSport;
+  let guss = 0;
+  while (guss < 6) {
+    gussSport = prompt('Guess What is my favorite sport?');
+    if (gussSport === arr[0]) {
+      alert('Correct the Football is my favorite sport');
+      counter = counter + 1;
+      break;
 
+    }
+
+    if (gussSport === arr[1]) {
+      alert('Correct the Swimming is my favorite sport');
+      counter = counter + 1;
+      break;
+
+
+    }
+    if (gussSport === arr[2]) {
+      alert('Error the Basketball is not my favorite sport');
+
+    }
+    if (gussSport === arr[3]) {
+      alert('Error the Tennis is not my favorite sport');
+
+    }
+    if (gussSport === arr[4]) {
+      alert('Error the Golf is not my favorite sport');
+
+    }
+    if (gussSport === arr[5]) {
+      alert('Error the Boxing is not my favorite sport');
+
+    }
+    if (gussSport === arr[6]) {
+      alert('Error the Bowling is not my favorite sport');
+
+    }
+    if (gussSport === arr[7]) {
+      alert('Correct the Horse Racing is my favorite sport');
+      counter = counter + 1;
+      break;
+
+
+    }
+    if (gussSport === arr[8]) {
+      alert('Error the Judo is not my favorite sport');
+
+
+    }
+    ++guss;
   }
 
-  if (gussSport === arr[1]) {
-    alert('Correct the Swimming is my favorite sport');
-    counter = counter + 1;
-    break;
-
+  if (gussSport !== arr[0] && gussSport !== arr[1] && gussSport !== arr[7]) {
+    alert('You have exhausted all your attempts');
 
   }
-  if (gussSport === arr[2]) {
-    alert('Error the Basketball is not my favorite sport');
-
-  }
-  if (gussSport === arr[3]) {
-    alert('Error the Tennis is not my favorite sport');
-
-  }
-  if (gussSport === arr[4]) {
-    alert('Error the Golf is not my favorite sport');
-
-  }
-  if (gussSport === arr[5]) {
-    alert('Error the Boxing is not my favorite sport');
-
-  }
-  if (gussSport === arr[6]) {
-    alert('Error the Bowling is not my favorite sport');
-
-  }
-  if (gussSport === arr[7]) {
-    alert('Correct the Horse Racing is my favorite sport');
-    counter = counter + 1;
-    break;
-
-
-  }
-  if (gussSport === arr[8]) {
-    alert('Error the Judo is not my favorite sport');
-
-
-  }
-  ++guss;
-}
-
-if (gussSport !== arr[0] && gussSport !== arr[1] && gussSport !== arr[7]) {
-  alert('You have exhausted all your attempts');
-
-}
 };
 mySport();
 
